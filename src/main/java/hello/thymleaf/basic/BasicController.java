eeb81c7b2795c7b9cb2ac74a234adbefa347b01d
imsori <dlathfl868@naver.com>
2022-12-20 14:49:12 +0900

    th:text

---
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -0,0 +1,22 @@
+package hello.thymleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @RequestMapping("/text-basic")
+    public String textBasic(Model model){
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @RequestMapping("/text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-unescaped";
+    }
+}
